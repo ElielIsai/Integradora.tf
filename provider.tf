5ab6c00a08bd404c8b7e4ce3fa1d101f6af7f12b
Uziel Vasquez <20243ds096@utez.edu.mx>
2026-04-11 23:35:47 -0600

    como no subi el archivo terraform.tfstate, la memoria no se guarda, asi que la menajare en s3

--- provider.tf
+++ provider.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.0"
     } 
   }
+  backend "s3" {
+    bucket = "estado-terraform" 
+    key    = "infraestructura/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
