843ef1373d55d99bcb490ac17a721fea16c08420
Uziel Vasquez <20243ds096@utez.edu.mx>
2026-04-13 04:07:50 -0600

    quite el nat repetido

--- route_tables.tf
+++ route_tables.tf
@@ -55,10 +55,3 @@ resource "aws_route_table_association" "private_assoc_2" {
   subnet_id      = aws_subnet.private_subnet_2.id
   route_table_id = aws_route_table.private_rt.id
 }
-
-# Agregar ruta a internet para la tabla de ruteo privada a través del NAT Gateway
-resource "aws_route" "private_nat" {
-  route_table_id         = aws_route_table.private_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.main.id
-}
